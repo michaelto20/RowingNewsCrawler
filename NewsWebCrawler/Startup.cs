7269a416038d2450e6ece2520fc3a4d993e4ca1f
michael_townsend <michael.h.townsend@gmail.com>
2016-10-20 17:08:44 -0400

    initial commit

---
+++ NewsWebCrawler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsWebCrawler.Startup))]
+namespace NewsWebCrawler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
