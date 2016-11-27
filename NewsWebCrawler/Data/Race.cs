eb5da75bd4277b3f3324f98b7728ec0b63b70b8e
michael_townsend <michael.h.townsend@gmail.com>
2016-11-26 22:49:03 -0500

    working version for milestone 2 - extra files

---
+++ NewsWebCrawler/Data/Race.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewsWebCrawler.Data
+{
+    public class Race
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Team { get; set; }
+
+        [Required]
+        public string Regatta { get; set; }
+        
+        // Navigation Properties
+        public virtual ICollection<Result> Results { get; set; }
+
+        public Race()
+        {
+
+        }
+     
+    }
+
+}
---
+++ NewsWebCrawler/Data/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewsWebCrawler.Data
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string FavTeam { get; set; }
+    }
+}
